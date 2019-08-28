8f5a4fe5a5823e7c22cc96bf81ed0bb04dba2138
Darek Najder <losman1987naj@gmail.com>
2019-08-28 21:30:29 +0200

    add project structur

---
+++ src/main/java/end_project/new_meetup/NewMeetupApplication.java
@@ -0,0 +1,13 @@
+package end_project.new_meetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMeetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMeetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/end_project/new_meetup/NewMeetupApplicationTests.java
@@ -0,0 +1,16 @@
+package end_project.new_meetup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewMeetupApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
