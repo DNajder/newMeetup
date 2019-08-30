4226ddca1326fde2dcd677f8cdb95e4be37da113
Darek Najder <losman1987naj@gmail.com>
2019-08-30 10:56:50 +0200

    add logging and adding to the database functionality.

--- src/main/java/end_project/new_meetup/config/BasicSecurityConfig.java
+++ src/main/java/end_project/new_meetup/config/BasicSecurityConfig.java
@@ -1,16 +1,45 @@
 package end_project.new_meetup.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests()
+                .antMatchers("/home", "home").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().loginPage("/login")
+                .loginProcessingUrl("/login-process")
+                .usernameParameter("emial")
+                .passwordParameter("password")
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/home");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT u.email, u.password_hash, 1 FROM user u WHERE u.email = ?")
+                .authoritiesByUsernameQuery("SELECT u.email, u.password_hash, 1 FROM user u WHERE u.email = ?")
+                .passwordEncoder(passwordEncoder);
     }
 }
--- src/main/java/end_project/new_meetup/controller/RegisterFormController.java
+++ src/main/java/end_project/new_meetup/controller/RegisterFormController.java
@@ -1,17 +1,25 @@
 package end_project.new_meetup.controller;
 
+import end_project.new_meetup.dao.UserModel;
 import end_project.new_meetup.dto.UserRegisterDTO;
 import end_project.new_meetup.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class RegisterFormController {
 
    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping({"/registration", "registration"})
     public String showUserRegisterForm(Model model) {
@@ -19,5 +27,17 @@ public class RegisterFormController {
         model.addAttribute("userRegisterDTO", userRegisterDTO);
         return "registerForm";
     }
+    @PostMapping({"/registration", "registration"})
+    public String UserRegisterDtoExtender(@ModelAttribute @Valid UserRegisterDTO userRegisterDTO, BindingResult bindingResult, Model model) {
 
+        if (bindingResult.hasErrors()){
+            return "registration";
+        }
+
+        UserModel userModel= new UserModel();
+        userModel.setName(userRegisterDTO.getName());
+        userModel.setEmail(userRegisterDTO.getEmail());
+        userModel.setPasswordHash(passwordEncoder.encode(userRegisterDTO.getPassword()));
+           return "redirect:/registrationSuccess";
+    }
 }
